90cd7c44a456a7e1765b4cc34e332fbf763be7fe
giorgio-marzo <gio.marzo96@gmail.com>
2025-05-05 20:09:48 +0200

    [UPD] compilato Header ed esportato in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import Header from './components/header/header'
 
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>Prova</h1>
+      <Header />
     </>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const header = () => {
     return (
-        <div>prova header</div>
+
+        <header>
+            <div className="container">
+                <div className="row d-flex al-center">
+                    <div className="col-50">
+                        <img src="src/assets/img/dc-logo.png" alt="Logo" />
+                    </div>
+                    <div className="col-60">
+                        <ul className='d-flex space-bet'>
+                            <li><a href="#">CHARACTERS</a></li>
+                            <li><a className='active' href="#">COMICS</a></li>
+                            <li><a href="#">MOVIES</a></li>
+                            <li><a href="#">TV</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+
+        </header>
+
+
     )
 }
 
